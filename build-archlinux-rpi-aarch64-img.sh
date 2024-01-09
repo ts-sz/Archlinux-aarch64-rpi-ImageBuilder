ae87e0b9cc5cb7420fcb587bb3d1745c6b16e0fd
valerius <v@strategic.zone>
2024-01-09 17:40:56 +0100

    check mode and install eeprom for specific model

--- build-archlinux-rpi-aarch64-img.sh
+++ build-archlinux-rpi-aarch64-img.sh
@@ -38,6 +38,7 @@ archlinuxarm="http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-${arm_version}-late
 archlinuxarm_md5="http://os.archlinuxarm.org/os/ArchLinuxARM-rpi-${arm_version}-latest.tar.gz.md5"
 
 LOOP_DEVICE="${1}"
+RPI_MODEL=$2
 
 # Check if the disk exists
 if [ ! -b "$LOOP_DEVICE" ]; then
@@ -127,6 +128,16 @@ arch-chroot $workdir/root pacman -Syu --noconfirm archlinux-keyring
 echo "Installing packages..."
 # Install packages
 arch-chroot $workdir/root pacman -S --noconfirm base-devel dosfstools git mkinitcpio-utils neovim nftables openssh python qrencode rsync sudo tailscale uboot-tools unzip zerotier-one zsh
+
+echo "Installing linux-${$RPI_MODEL} kernel and eeprom..."
+model=$(tr -d '\0' < /sys/firmware/devicetree/base/model)
+echo "Model: $model"
+if [[ $RPI_MODEL == 4 ]]
+then
+  arch-chroot $workdir/root pacman -S --noconfirm rpi4-eeprom
+elif [[ $RPI_MODEL == 5 ]]
+  arch-chroot $workdir/root pacman -S --noconfirm rpi5-eeprom
+fi
 
 echo "Setup hostname..."
 # Set the hostname
