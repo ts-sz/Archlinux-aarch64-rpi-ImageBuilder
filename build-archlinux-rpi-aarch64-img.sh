b008b0deaad6365de3b2a911e23debf098d78fd0
valerius <v@strategic.zone>
2024-01-09 16:10:01 +0100

    add check dist

--- build-archlinux-rpi-aarch64-img.sh
+++ build-archlinux-rpi-aarch64-img.sh
@@ -19,10 +19,17 @@ IF9fLyAKX19fXy8gXF9ffCBffCAgIFxfXyxffCBcX198IFxfX198IFxfXywgfCBffCBcX19ffCAg
 IF9fX3wgXF9fXy8gIF98ICBffCBcX19ffCAKICAgICAgICAgICAgICAgICAgICAgICAgICAgICAg
 ICAgIHxfX18vICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAgICAK" | base64 -d
 
+# Check if the user is root
+if [[ $EUID -ne 0 ]]; then
+  echo "This script must be run as root"
+  exit 1
+fi
+
 # Restart teh service systemd-binfmt.service
 echo "Restarting systemd-binfmt.service..."
 systemctl restart systemd-binfmt.service
 
+echo "Setting variables..."
 rpi_hostname="sz-rpi-aarch64-99"
 
 arm_version="aarch64"
@@ -38,6 +45,7 @@ if [ ! -b "$LOOP_DEVICE" ]; then
   exit 1
 fi
 workdir="/tmp/archlinux-aarch64-rpi-builder_$(date +%Y%m%d%H%M%S)/$arm_version"
+echo "Working directory: $workdir"
 mkdir -p $workdir
 
 # Define default locale and keymap settings
